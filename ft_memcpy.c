37da30b7eb904a406876c52eff469cb60de64299
jwoo9928 <astro9928@o.cnu.ac.kr>
2020-04-15 18:51:33 +0900

    fix

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char		*ptr;
 	const unsigned char	*ptr2;
 
-	if (num == 0 || dst == src)
+	if (n == 0 || dst == src)
 		return (dst);
 	ptr = (unsigned char*)dst;
 	ptr2 = (unsigned char*)src;
